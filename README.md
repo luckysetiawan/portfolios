a2f6061020225d01ae3cf18d944adbb1dcc1c048
luckysetiawan <luckysetiawan0802@gmail.com>
2023-12-22 14:35:40 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Portofolios
+
+This is my portofolios. This project is part of [my presonal website](https://luckysetiawan.github.io/) website. You can visit my website by clicking the link below
+
+[![https://luckysetiawan.github.io/](https://img.shields.io/badge/website-000000?style=for-the-badge&logo=About.me&logoColor=white)](https://luckysetiawan.github.io/)
+
+### Additional tech used:
+* [reveal.js](https://revealjs.com/)
+* [reveal.js-menu](https://github.com/denehyg/reveal.js-menu)
+* [Google Fonts](https://fonts.google.com/)
+* [particles.js](https://vincentgarreau.com/particles.js/)
+* [ICONS8](https://icons8.com/)
---
+++ js/app.js
@@ -0,0 +1,136 @@
+// Initialize reveal.js
+Reveal.initialize({
+  hash: true,
+  jumpToSlide: false,
+  history: true,
+  overview: false,
+  mouseWheel: true,
+  help: false,
+  scrollActivationWidth: null,
+  
+  plugins: [ RevealMenu ],
+
+  menu: { markers: false }
+});
+
+// Initialize particle.js
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 30,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#a6fbf5"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#a6fbf5"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#c3b5fa",
+        "opacity": 0.4,
+        "width": 3
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
