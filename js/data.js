f3e376f1dd0b8045a86f431833d3ad2e2437d6a5
luckysetiawan <luckysetiawan0802@gmail.com>
2024-01-25 12:01:06 +0700

    added professional experience job description and responsibilities

--- js/data.js
+++ js/data.js
@@ -12,7 +12,17 @@ const professionalExpeiences = [
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">Anugrah Abadi</span></h2>
 
-      <p>A roofing company that recently embraced digital transformation to redefine roofing buying experience.</p>
+      <p>A leading roofing roofing materials company that provides the necessaary materials to build a good roof. It is a company that recently embraced digital transformation to redefine the roofing buying experience.</p>
+    </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">Anugrah Abadi</span></h2>
+      
+      <p>Developed a web application that serves as a catalog and ordering portal for the company.</p>
+      <ul>
+        <li>Designed and created the front end website.</li>
+        <li>Engineered tailor-made UI components to enhance the user interface and code maintainability.</li>
+        <li>Successfully integrated Google Workspace API for data storage.</li>
+      </ul>
     </section>
   `,
   `
@@ -31,6 +41,22 @@ const professionalExpeiences = [
 
       <p>One of the greatest digital marketing companies in Indonesia that offers small and medium-sized enterprise services to help market their products better such as Marketplace Management, Social Media Management, Teaching and Coaching, etc.</p>
     </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">MEA Digital Marketing</span></h2>
+      
+      <p>Developed a new marketplace application and maintained the server-side applications and databases that when combined with front end codes, help create a functional, seamless experience for the end user.</p>
+    </section>
+    <section>
+      <ul>
+        <li>Migrated source codes to clean architecture.</li>
+        <li>Refactored source codes to add an extra architecture layer.</li>
+        <li>Created new features to support business processes.</li>
+        <li>Tested, debugged, and updated existing functions.</li>
+        <li>Involved in the design team in designing the database schema for the new app.</li>
+        <li>Improved test coverage.</li>
+        <li>Successfully integrated four external 3rd party APIs into the system.</li>
+      </ul>
+    </section>
   `,
   `
     <section data-auto-animate>
@@ -44,12 +70,27 @@ const professionalExpeiences = [
       <icon-component logo="springboot"></icon-component>
       <icon-component logo="postgresql"></icon-component>
       <icon-component logo="redis"></icon-component>
+      <icon-component logo="docker"></icon-component>
       <icon-component logo="thymeleaf"></icon-component>
     </section>
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">Systeric</span></h2>
 
       <p>A software development agency that offers information technology services and consulting that will take your ideas from ideation to production. It is a fast-growing startup that concentrates on mobile, web, and product development, as well as systems integration.</p>
+    </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">Systeric</span></h2>
+      
+      <p>Developed web-based applications and web services using various programming languages and frameworks.</p>
+    </section>
+    <section>
+      <ul>
+        <li>Created new features to support business processes.</li>
+        <li>Tested, debugged, and updated existing functions.</li>
+        <li>Improved front end display layout and displayed text.</li>
+        <li>Refactored source codes entity handling logic.</li>
+        <li>Collaborated with the design team for the PDF export feature.</li>
+      </ul>
     </section>
   `,
 ];
