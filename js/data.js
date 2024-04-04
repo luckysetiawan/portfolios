cb1fe788e712a316eeb80f950bb6d990b8857154
luckysetiawan <luckysetiawan0802@gmail.com>
2024-04-04 15:24:24 +0700

    updated professional experiences data

--- js/data.js
+++ js/data.js
@@ -12,15 +12,30 @@ const professionalExpeiences = [
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">Anugrah Abadi</span></h2>
 
-      <p>A leading roofing roofing materials company that provides the necessaary materials to build a good roof. It is a company that recently embraced digital transformation to redefine the roofing buying experience.</p>
+      <p><b>Company Profile</b></p>
+      <p>A leading roofing materials provider that recently embraced digital transformation to redefine the roofing buying experience.</p>
+    </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">Anugrah Abadi</span></h2>
+
+      <p><b>Scope of Responsibility</b></p>
+      <p>Developed a web application that serves as a catalog and ordering portal for the company.</p>
     </section>
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">Anugrah Abadi</span></h2>
       
-      <p>Developed a web application that serves as a catalog and ordering portal for the company.</p>
+      <p><b>Responsibilities and Achievements (1/2)</b></p>
       <ul>
-        <li>Designed and created the front end website.</li>
-        <li>Engineered tailor-made UI components to enhance the user interface and code maintainability.</li>
+        <li>Conducted stakeholder meetings to gather requirements and ensure alignment with functionality and design prototypes.</li>
+        <li>Designed and developed the front end website layout and design.</li>
+      </ul>
+    </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">Anugrah Abadi</span></h2>
+      
+      <p><b>Responsibilities and Achievements (2/2)</b></p>
+      <ul>
+        <li>Designed and implemented custom UI components to enhance the UI and code maintainability.</li>
         <li>Successfully integrated Google Workspace API for data storage.</li>
       </ul>
     </section>
@@ -39,21 +54,34 @@ const professionalExpeiences = [
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">MEA Digital Marketing</span></h2>
 
-      <p>One of the greatest digital marketing companies in Indonesia that offers small and medium-sized enterprise services to help market their products better such as Marketplace Management, Social Media Management, Teaching and Coaching, etc.</p>
+      <p><b>Company Profile</b></p>
+      <p>One of the greatest digital marketing companies in Indonesia, specializing in services for small and medium-sized enterprises to enhance their product marketing efforts.</p>
+    </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">MEA Digital Marketing</span></h2>
+
+      <p><b>Scope of Responsibility</b></p>
+      <p>Developed a new marketplace application and maintained server-side applications and databases.</p>
     </section>
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">MEA Digital Marketing</span></h2>
       
-      <p>Developed a new marketplace application and maintained the server-side applications and databases that when combined with front end codes, help create a functional, seamless experience for the end user.</p>
-    </section>
-    <section>
+      <p><b>Responsibilities and Achievements (1/2)</b></p>
       <ul>
-        <li>Migrated source codes to clean architecture.</li>
+        <li>Developed the new marketplace application with other developers.</li>
+        <li>Migrated source codes to clean architecture, tripled code maintainability for 30% of the modules.</li>
         <li>Refactored source codes to add an extra architecture layer.</li>
-        <li>Created new features to support business processes.</li>
+        <li>Developed new features to support business processes.</li>
+      </ul>
+    </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">MEA Digital Marketing</span></h2>
+
+      <p><b>Responsibilities and Achievements (2/2)</b></p>
+      <ul>
         <li>Tested, debugged, and updated existing functions.</li>
-        <li>Involved in the design team in designing the database schema for the new app.</li>
-        <li>Improved test coverage.</li>
+        <li>Designed the database schema for the new marketplace app with the design team.</li>
+        <li>Improved test coverage across multiple modules to cover more scenarios.</li>
         <li>Successfully integrated four external 3rd party APIs into the system.</li>
       </ul>
     </section>
@@ -76,20 +104,32 @@ const professionalExpeiences = [
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">Systeric</span></h2>
 
-      <p>A software development agency that offers information technology services and consulting that will take your ideas from ideation to production. It is a fast-growing startup that concentrates on mobile, web, and product development, as well as systems integration.</p>
+      <p><b>Company Profile</b></p>
+      <p>A software development agency specializing in IT services and consulting. Offers mobile, web, and product development, as well as systems integration.</p>
     </section>
     <section data-auto-animate>
       <h2><span class="rainbow_text_animated">Systeric</span></h2>
-      
+
+      <p><b>Scope of Responsibility</b></p>
       <p>Developed web-based applications and web services using various programming languages and frameworks.</p>
     </section>
-    <section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">Systeric</span></h2>
+
+      <p><b>Responsibilities and Achievements (1/2)</b></p>
       <ul>
         <li>Created new features to support business processes.</li>
         <li>Tested, debugged, and updated existing functions.</li>
         <li>Improved front end display layout and displayed text.</li>
-        <li>Refactored source codes entity handling logic.</li>
-        <li>Collaborated with the design team for the PDF export feature.</li>
+      </ul>
+    </section>
+    <section data-auto-animate>
+      <h2><span class="rainbow_text_animated">Systeric</span></h2>
+      
+      <p><b>Responsibilities and Achievements (2/2)</b></p>
+      <ul>
+        <li>Refactored source codes entity handling logic, reduced external dependencies.</li>
+        <li>Designed and implemented the layout for the PDF export feature with the design team.</li>
       </ul>
     </section>
   `,
